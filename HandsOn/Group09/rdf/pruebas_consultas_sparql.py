9d65a3ac70857d2f5729b20a0ff35a1b91ad74ae
germaangil <germangilplanes@gmail.com>
2025-10-20 14:48:15 +0200

    Versión mejorada del rdf de microclimate

--- HandsOn/Group09/rdf/pruebas_consultas_sparql.py
+++ HandsOn/Group09/rdf/pruebas_consultas_sparql.py
@@ -4,6 +4,8 @@ g = Graph()
 g.parse("microclimate-sensors-data-enriched-updated.ttl", format="ttl")
 
 q = """
+PREFIX us: <https://smartcity.linkeddata.es/lcc/ontology/urban-sensors#>
+
 SELECT ?sensor
 WHERE {
   ?sensor a us:MicroclimateSensor .
