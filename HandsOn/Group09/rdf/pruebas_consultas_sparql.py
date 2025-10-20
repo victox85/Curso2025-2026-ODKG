12b7e7d60f4d76c84bd89f880bb9162b0dbc27f2
pauhermig02 <paulaherrero02@hotmail.com>
2025-10-20 21:32:44 +0200

    Localizaciones y sensores pedestrian

--- HandsOn/Group09/rdf/pruebas_consultas_sparql.py
+++ HandsOn/Group09/rdf/pruebas_consultas_sparql.py
@@ -1,15 +1,20 @@
 from rdflib import Graph
 
 g = Graph()
-g.parse("microclimate-sensors-data-enriched-updated.ttl", format="ttl")
+g.parse("C:/Users/paula/Desktop/Master/Primer_cuatri/Open Data and Knowledge Graphs/Curso2025-2026-ODKG/HandsOn/Group09/rdf/pedestrian-counting-system-sensor-locations-enriched-updated.ttl", format="ttl")
 
 q = """
-SELECT ?sensor
+PREFIX wgs84_pos: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX us: <https://smartcity.linkeddata.es/lcc/ontology/urban-sensors#>
+
+SELECT ?location ?lat ?long
 WHERE {
-  ?sensor a us:MicroclimateSensor .
+  ?location a us:Location ;
+            wgs84_pos:lat ?lat ;
+            wgs84_pos:long ?long .
 }
+LIMIT 5
 """
 
-results = g.query(q)
-for row in results:
+for row in g.query(q):
     print(row)
